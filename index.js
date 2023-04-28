537ac9280d12d0552d888b6a251465d8af4601d3
shawon012 <shawon.shakib01@gmail.com>
2023-04-28 23:30:55 +0600

    today

--- index.js
+++ index.js
@@ -7,19 +7,38 @@ const news = require('./data/news.json')
 
 app.use(cors());
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('Dragon is running');
 });
 
-app.get('/catagories', (req, res) =>{
+app.get('/catagories', (req, res) => {
     res.send(catagories);
 })
 
-app.get('/news', (req, res) =>{
+app.get('/news', (req, res) => {
     res.send(news);
 })
 
-app.listen(port, ()=>{
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    if (id == 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+
+})
+
+app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
 })
 
@@ -27,7 +46,7 @@ app.listen(port, ()=>{
 
 
 
- 
+
 
 
 // echo "# the-news-dragon-server" >> README.m
